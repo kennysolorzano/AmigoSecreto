d71ce20c278ccf849a4c6364f2d4486d40f3c964
Kenny <kennysolo@gmail.com>
2025-02-17 20:28:00 -0400

    función para agregar amigos

--- app.js
+++ app.js
@@ -3,6 +3,19 @@
 let amigos = [];
 
 //Implementa una función para agregar amigos
+function agregarAmigo() {
+    const input = document.getElementById("amigo");
+    const nombre = input.value.trim();
+
+    if (nombre === "") {
+        alert("Por favor, ingrese un nombre válido.");
+        return;
+    }
+
+    amigos.push(nombre);
+    actualizarLista();
+    input.value = "";
+}
 
 //Implementa una función para actualizar la lista de amigos
 
